e76aa3da2fed4f05bc6e0b6b222078a6787c560f
Gustavo Correa <gustavocorrea66@gmail.com>
2022-01-05 16:51:29 -0300

    adicionado um segundo dado

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rolldice
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,18 +14,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.buttom)
-        rollButton.setOnClickListener { rollDice() }
-        rollDice()
+
+
+        rollButton.setOnClickListener {
+            val newdice = Dice(6)
+            val diceView: ImageView = findViewById(R.id.diceView)
+            rollDice(newdice, diceView)
+
+            val diceTwo = Dice(6)
+            val diceViewTwo: ImageView = findViewById(R.id.diceView2)
+            rollDice(diceTwo, diceViewTwo)
+        }
+
     }
 
     // gira o dado e faz updade na tela com o resultado
-    private fun rollDice() {
-        val dice = Dice(6)
+    private fun rollDice(dice: Dice, image: ImageView) {
+        //val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        val diceImage: ImageView = findViewById(R.id.diceView)
+        val diceImage: ImageView = image
 
-        //determina qual imagem vai ser usada no resultado
+        //determina qual imagem vai ser usada baseada no resultado do roll
         val drawaleResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -34,11 +45,14 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
+        //da um update na descricao do conteudo (para acessibilidade)
         diceImage.contentDescription = diceRoll.toString()
+
+        //da um update no imageview com a imagem correta do dado
         diceImage.setImageResource(drawaleResource)
     }
 }
 
 class Dice(private val numSides: Int) {
-    fun roll(): Int = (1..6).random()
+    fun roll(): Int = (1..numSides).random()
 }
